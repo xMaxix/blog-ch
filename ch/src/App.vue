b2ac274cb67f0ceb1bb119ced28a979614e00600
Maximilian Schatz <510max.schatz@gmx.at>
2023-06-03 23:45:49 +0200

    asdf

---
+++ ch/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+<style>
+@font-face {
+  font-family: "Merienda";
+  src: local("Merienda"),
+   url(./fonts/BebasNeue-Regular.ttf) format("truetype");
+}
+</style>
+<script setup>
+</script>
---
+++ ch/src/views/h.vue
@@ -0,0 +1,8 @@
+<template>
+    <HelloWorld />
+  </template>
+  
+  <script setup>
+    import HelloWorld from '@/components/homeSel.vue'
+  </script>
+  
